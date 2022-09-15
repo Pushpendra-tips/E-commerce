ae01302ed2fd178614c965c8d1d1ed14ed2a11e8
Pushpendra Tripathi <trippushpendra195@gmail.com>
2022-09-16 04:32:18 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose')
-
-mongoose.connect("mongodb+srv://ujjwal:ujjwal@cluster0.hdo3ucc.mongodb.net/shop?retryWrites=true&w=majority")
+const dotenv = require ("dotenv");
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("DB Connection SuccessFull!"))
 .catch((err)=>{
 console.log(err)
 })
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT|| 5000, ()=>{
 console.log("Backend Server is running!")
 });
