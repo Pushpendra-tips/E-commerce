b9c77ec37a0aaf5d0988359bd38a8657d40d3ccb
Pushpendra Tripathi <trippushpendra195@gmail.com>
2022-09-16 02:38:15 +0530

    login ,signup, cart done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <div>
-      <Product/>
+      {/* <Register/> */}
+      {/* <Login/> */}
+      <Cart/>
     </div>
   );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,34 @@
+import { Badge } from "@mui/icons-material";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
-import { Search,ShoppingCartOutlined} from "@mui/icons-material";
-import Badge from '@mui/material/Badge';
+import { mobile } from "../responsive";
+
 const Container = styled.div`
-    height: 60px;
-  `;
+  height: 60px;
+  ${mobile({ height: "50px" })}
+`;
 
-  const Wrapper = styled.div`
-    padding: 10px 20px;
-    display:flex;
-    justify-content:space-around;
-    align-items:center;
-  `;
-  const Left = styled.div`
-  flex:1;
-  display:flex;
-  align-items:center;
-  `;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
+`;
 
-const Language = styled.div `
-font-size:14px;
-cursor:pointer;
-`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
+`;
+
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
   display: flex;
@@ -29,50 +36,58 @@ const SearchContainer = styled.div`
   margin-left: 25px;
   padding: 5px;
 `;
-  const Center = styled.div`
-  flex:1; text-align:center;
-  `;
-  const Right = styled.div`
-  flex:1;
-  align-items:center;
-  justify-content:flex-end;
-  display:flex;
-  `;
 
-  const Input = styled.input`
-  border: none;`;
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
 
-  const Logo = styled.h1`
-  font-weight:bold;
-  `
-const MenuItem = styled.div `
-font-size:14px;
-cursor:pointer;
-margin-left:25px;
-`
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`;
 
 const Navbar = () => {
-  
   return (
     <Container>
       <Wrapper>
         <Left>
-            <Language>EN</Language>
-            <SearchContainer>
+          <Language>EN</Language>
+          <SearchContainer>
             <Input placeholder="Search" />
             <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
-        <Center><Logo>UjjwalTIPS.</Logo></Center>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
         <Right>
-
-<MenuItem>REGISTER</MenuItem>
-<MenuItem>SIGN IN</MenuItem>
-<MenuItem>
-<Badge badgeContent={4} color="primary">
-<ShoppingCartOutlined />
-    </Badge>
-</MenuItem>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
         </Right>
       </Wrapper>
     </Container>
