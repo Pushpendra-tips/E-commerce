c5c8ecb58dd021ebcb1ddfbd2177032a5894dfe4
Pushpendra Tripathi <trippushpendra195@gmail.com>
2022-09-16 02:08:40 +0530

    single product done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 
 const App = () => {
   return (
     <div>
-      <ProductList/>
+      <Product/>
     </div>
   );
 };
